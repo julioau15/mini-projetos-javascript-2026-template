a0f4ebf32693205e83ae5c0404947afa3d584cbd
JULIO AUGUSTO BARBOSA DA SILVA <25203694@CFP127ALUNO.LOCAL>
2026-03-12 11:27:20 -0300

    correções nos exercios 01 e 02

--- 01-teste-aptidao/main.js
+++ 01-teste-aptidao/main.js
@@ -2,19 +2,19 @@
 
 // verifica se esta apto ou não
 const testarAptidao = () => {
-    let tempo = document.getElementById('input-tempo').value
+    let tempo = document.getElementById('input-tempo')
     let container = document.getElementById('resultado')
     let resultado
     let cor
     
 
     // validação
-    if(!isvalid(tempo)){
+    if(!isValid(tempo.value) || !isLimit(0,100,tempo.value)){
         alert('ERRO! Por favor, digite um valor válido.')
         return
     }else{
         // define resultado e cor
-        if(Number(tempo) < 14){
+        if(Number(tempo.value) < 14){
             resultado = 'Apto'
             cor = '#14532d'
         }else{
@@ -23,17 +23,19 @@ const testarAptidao = () => {
         }
 
         // chama escrever mensagem
-        escreverMensagem(resultado, container, cor)
+        escreverMensagem(resultado, container, tempo.value, cor)
+        limparConteudo(tempo)
+        tempo.focus()
     }
 }
 
 // escreve uma mensagem em algum container
-const escreverMensagem = (mensagem, container, cor=null) => {
+const escreverMensagem = (mensagem, container, tempo, cor=null) => {
     // limpa o conteudo do container
     container.textContent = ''
     // cria um paragrafo
     let paragrafo = document.createElement('p')
-    paragrafo.textContent = mensagem
+    paragrafo.textContent = `Com o tempo de ${tempo} você está ${mensagem}`
 
     // caso não seja passado uma cor, ele não modifica
     if(cor != null)
@@ -44,10 +46,10 @@ const escreverMensagem = (mensagem, container, cor=null) => {
 }
 
 // verifica se um dado é valido
-const isvalid = (dado) => {
-    if(isNaN(dado) || dado.trim() == '' || dado == undefined || dado == null){
-        return false
-    }else{
-        return true
-    }
-}
+const isValid = (...d) => d.every(dado => !isNaN(dado) && dado.trim() != '' && dado != undefined && dado != null)
+
+// valida se um numero esta no limite definido
+const isLimit = (min, max, ...n) => n.every(numero => numero >= 0 && numero <= 100)
+
+// limpa o conteudo de um campo
+const limparConteudo = (...d) =>d.forEach(dado => dado.value = '')
--- 02-calculadora-desconto/main.js
+++ 02-calculadora-desconto/main.js
@@ -1,32 +1,34 @@
 'use strict'
 
 const calcularDesconto = () => {
-    let precoOriginal = document.getElementById("input-preco-original").value
-    let desconto = document.getElementById("input-desconto").value
+    let precoOriginal = document.getElementById("input-preco-original")
+    let desconto = document.getElementById("input-desconto")
     let container = document.getElementById('resultado')
     let precoFinal
     let valorEconomizado
     let cor
 
-    if(!isvalid(precoOriginal) || !isvalid(desconto)){
+    if(!isValid(precoOriginal.value, desconto.value) || !isLimit(0,100,desconto.value)){
         alert('ERRO! Por favor, digite um valor válido.')
         return
     }else{
-        precoFinal =  Number(precoOriginal) - (Number(precoOriginal) * (Number(desconto) / 100))
-        valorEconomizado = (Number(precoOriginal) - precoFinal).toFixed(2)
-        cor = definirCor(desconto)
-        escreverMensagem(`Com um desconto de ${desconto}% Você economizou R\$${valorEconomizado}.`, container, cor)
+        precoFinal =  Number(precoOriginal.value) - (Number(precoOriginal.value) * (Number(desconto.value) / 100))
+        valorEconomizado = (Number(precoOriginal.value) - precoFinal).toFixed(2)
+        cor = definirCor(desconto.value)
+        escreverMensagem(desconto.value,valorEconomizado, container, cor)
+        limparConteudo(precoOriginal, desconto)
+        precoOriginal.focus()
     }
 }
 
 // verifica se um dado é valido
-const isvalid = (dado) => {
-    if(isNaN(dado) || dado.trim() == '' || dado == undefined || dado == null){
-        return false
-    }else{
-        return true
-    }
-}
+const isValid = (...d) => d.every(dado => !isNaN(dado) && dado.trim() != '' && dado != undefined && dado != null)
+
+// valida se um numero esta no limite definido
+const isLimit = (min, max, ...n) => n.every(numero => numero >= 0 && numero <= 100)
+
+// limpa o conteudo de um campo
+const limparConteudo = (...d) =>d.forEach(dado => dado.value = '')
 
 const definirCor = (n) => {
     let desconto = Number(n)
@@ -38,16 +40,19 @@ const definirCor = (n) => {
         return '#7f1d1d'
 }
 
-const escreverMensagem = (mensagem, container, cor=null) => {
+const escreverMensagem = (desconto, valorEco, container, cor=null) => {
     // limpa o conteudo do container
     container.textContent = ''
     // cria um paragrafo
     let paragrafo = document.createElement('p')
-    paragrafo.textContent = mensagem
+    let valorEconomizado = document.createElement('p')
+    valorEconomizado.innerText = `${valorEco}`
 
     // caso não seja passado uma cor, ele não modifica
     if(cor != null)
-        paragrafo.style.color = cor
+        valorEconomizado.style.color = cor
+
+    paragrafo.textContent = `Com um desconto de ${desconto}% Você economizou R\$${valorEconomizado.value}.`
 
     // adiciona o pagragrafo no container
     container.appendChild(paragrafo)
